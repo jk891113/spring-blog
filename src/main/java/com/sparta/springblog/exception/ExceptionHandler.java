74d695dc5f2fe3b814160c1652a2191a1a4f9be1
HanJungkyu <jk891113@naver.com>
2022-12-21 11:44:15 +0900

    ExceptionHandler 반환타입 ResponseEntity로 변경

--- src/main/java/com/sparta/springblog/exception/ExceptionHandler.java
+++ src/main/java/com/sparta/springblog/exception/ExceptionHandler.java
@@ -5,6 +5,7 @@ import com.sparta.springblog.responsedto.StatusResponseDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,23 +16,23 @@ import java.nio.charset.Charset;
 public class ExceptionHandler {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
-    public StatusResponseDto IllegalArgumentExceptionHandler(IllegalArgumentException e) {
+    public ResponseEntity<StatusResponseDto> IllegalArgumentExceptionHandler(IllegalArgumentException e) {
         StatusResponseDto responseDto = new StatusResponseDto();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
         responseDto.setStatus(StatusEnum.BAD_REQUEST);
         responseDto.setMessage(e.getMessage());
-        return responseDto;
+        return new ResponseEntity<>(responseDto, headers, HttpStatus.BAD_REQUEST);
     }
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
-    public StatusResponseDto MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+    public ResponseEntity<StatusResponseDto> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         StatusResponseDto responseDto = new StatusResponseDto();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
         responseDto.setStatus(StatusEnum.BAD_REQUEST);
         responseDto.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-        return responseDto;
+        return new ResponseEntity<>(responseDto, headers, HttpStatus.BAD_REQUEST);
     }
 }
